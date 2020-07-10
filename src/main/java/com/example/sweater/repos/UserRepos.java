2d50498000bb7c31b122cb4b19393f6af39f3b06
Oleh Kostyuk <oleh.Kostyuk081969@gmail.com>
2020-07-10 11:12:13 +0300

    fifth  commit after deleting sweater

--- src/main/java/com/example/sweater/repos/UserRepos.java
+++
@@ -1,9 +0,0 @@
-package com.example.sweater.repos;
-
-import com.example.sweater.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepos extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
